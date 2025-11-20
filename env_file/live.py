fdcd5a3e04332f5485ba599f6d4d8a783c4efba5
nihal <nihal@mitesolutions.com>
2025-11-20 13:21:39 +0000

    added new databse for company

--- env_file/live.py
+++ env_file/live.py
@@ -1,7 +1,7 @@
 # env_file/live.py
 
-DB_NAME = 'digidnextlivedb'
-DB_USER = 'digidnextlivedb_user'
-DB_PASSWORD = '33fIOLT6CfU76WFEjCuHHEsY7qsqw1Tt'
-DB_HOST = 'dpg-d4ctfa49c44c7391mvkg-a'
+DB_NAME = 'diginextaedb'
+DB_USER = 'diginextaedb_user'
+DB_PASSWORD = 'XPxVqW2bGesARzQuRNoqnht1oIYlxHMk'
+DB_HOST = 'dpg-d4fhb5uuk2gs73fc0dp0-a'
 DB_PORT = '5432'
