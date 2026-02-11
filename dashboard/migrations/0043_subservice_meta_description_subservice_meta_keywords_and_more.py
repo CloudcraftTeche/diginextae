3b1af70fb9f5cc9a3e26eb1d02b2e2be52b72d30
Ananthu Shaji <anandhushajiareeckal2002@gmail.com>
2026-02-11 13:45:24 +0530

    remove unique

--- dashboard/migrations/0043_subservice_meta_description_subservice_meta_keywords_and_more.py
+++ dashboard/migrations/0043_subservice_meta_description_subservice_meta_keywords_and_more.py
@@ -28,6 +28,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='subservice',
             name='slug',
-            field=models.SlugField(blank=True, max_length=255, unique=True),
+            field=models.SlugField(blank=True, max_length=255, null=True),
         ),
     ]
